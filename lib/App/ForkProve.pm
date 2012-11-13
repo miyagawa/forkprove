6144af84b87b3c2e44a5f5adbe9758dae54accb6
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-11-12 20:19:30 -0800

    Checking in changes prior to tagging of version v0.4.2.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d6dd2b1..343c8af 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension App::ForkProve
    
    +v0.4.2 Mon Nov 12 20:18:49 PST 2012
    +       - Support --merge (#9)
    +       - Fix Getopt::Long options to make options case sensitive
    +
     v0.4.1 Fri Nov  9 14:10:47 PST 2012
            - Clear @ARGV for each test (#8)

--- lib/App/ForkProve.pm
+++ lib/App/ForkProve.pm
@@ -2,7 +2,7 @@ package App::ForkProve;
 
 use strict;
 use 5.008_001;
-use version; our $VERSION = "v0.4.1";
+use version; our $VERSION = "v0.4.2";
 
 use App::Prove;
 use Getopt::Long ':config' => qw(pass_through no_ignore_case);
